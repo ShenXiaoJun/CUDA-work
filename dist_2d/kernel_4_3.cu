1178bb7c6799feb64b801d94a7960cce21306a40
shenxj <2200319548@qq.com>
2018-12-03 19:51:45 +0800

    提交计算二维距离的代码

---
+++ dist_2d/kernel_4_3.cu
@@ -0,0 +1,33 @@
+#define W 500
+#define H 500
+#define TX 32
+#define TY 32
+
+__global__
+void distanceKernel(float *d_out, int w,int h, float2 pos){
+	const int c = blockIdx.x*blockDim.x + threadIdx.x;
+	const int r = blockIdx.y*blockDim.y + threadIdx.y;
+	const int i = r*w + c;
+	if ((c >= w)||(r >= h)) return;
+
+	d_out[i] = sqrtf((c - pos.x)*(c - pos.x) + (r - pos.y)*(r - pos.y));
+
+}
+
+int main(){
+	float *out = (float *)calloc(W*H, sizeof(float));
+	float *d_out;
+	cudaMalloc(&d_out, W*H*sizeof(float));
+	
+	const float2 pos = {0.0f, 0.0f};
+	const dim3 blockSize(TX, TY);
+	const int bx = (W + TX - 1)/TX;
+	const int by = (W + TY - 1)/TY;
+	const dim3 gridSize = dim3(bx, by);
+
+	distanceKernel<<<gridSize, blockSize>>>(d_out, W, H, pos);
+	
+	cudaFree(d_out);
+	free(out);
+	return 0;
+}
